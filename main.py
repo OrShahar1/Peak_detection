61ad80e31acab8878e55f8d78308eb8623b98fad
OrShahar1 <63114851+OrShahar1@users.noreply.github.com>
2020-05-27 21:30:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import scipy.io.wavfile as wavfile
+import scipy
+import scipy.fftpack as fftpk
+from scipy.signal import find_peaks
+import numpy as np
+from matplotlib import pyplot as plt
+import sounddevice as sd
+import time
+
+s_rate, signal = wavfile.read("string.wav")
+
+FFT = abs(scipy.fft(signal))
+freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+
+# fft plot
+# look only on the positive freq
+plt.plot(freqs,FFT)
+plt.xlabel('Frequency HZ')
+plt.ylabel("Amplitude")
+plt.show()
+
+# detect peaks !
+# default of peak is 300000.
+min_peak_heigth = 300000
+indices = find_peaks(FFT[range(len(FFT)//2)], height=min_peak_heigth, width=2)[0]
+peaks = [freqs[x] for x in indices]
+
+
+plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])
+plt.scatter(peaks,[min_peak_heigth for x in peaks] , c='red')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.show()
+
+# length of the new sample
+duration = 5.0
+x = np.linspace(0, int(duration*2*np.pi), int(duration*float(s_rate)))
+
+wave_table = np.array(np.sin(peaks[0] * x))
+for i in peaks[1:]:
+      wave_table = wave_table + np.sin(i * x)
+
+
+
+
+
+wave_table = wave_table * 0.02
+sd.play(wave_table, s_rate)
+time.sleep(duration)
+sd.stop()
+
+
+plt.plot(wave_table)#plt.plot(signal)
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.show()
+
+
+
+
+wavfile.write("string_texture.wav", s_rate, wave_table)
+
